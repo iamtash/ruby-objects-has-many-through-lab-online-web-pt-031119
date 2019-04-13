1c90e2367afa78556a43824b8d0b9bb6f74eaf09
Natasha Chernis <natashachernis@gmail.com>
2019-04-13 01:22:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-class Song 
+class Song
 
   attr_accessor :name, :artist, :genre
 
@@ -8,7 +8,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    @@all << self 
+    @@all << self
   end
 
 end
