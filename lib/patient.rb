3c759aaa7ed33119fed4dc5d04b4e59b8211119a
Natasha Chernis <natashachernis@gmail.com>
2019-04-13 01:41:04 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,4 +13,11 @@ class Patient
     @@all
   end
 
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select {|appt| appt.patient == self}
+  end
 end
